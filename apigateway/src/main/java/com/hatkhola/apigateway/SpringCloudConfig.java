8d79f03025486bc992a17fa6b5a41df9bfa18e4c
faysal <faysal.ahmad@mislbd.com>
2021-09-14 18:43:03 +0600

    resilience4j added to the spring cloud api gateway

--- apigateway/src/main/java/com/hatkhola/apigateway/SpringCloudConfig.java
+++ apigateway/src/main/java/com/hatkhola/apigateway/SpringCloudConfig.java
@@ -2,10 +2,12 @@ package com.hatkhola.apigateway;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableHystrix
 public class SpringCloudConfig{
 
     @Bean
